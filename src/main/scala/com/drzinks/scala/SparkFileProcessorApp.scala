0b8aa25994e5796b50ee61df1ad543c06c5a7313
drzinks <you@example.com>
2024-01-21 15:33:21 +0100

    scala,java,basic tranformations in spark,logback : squashed

--- src/main/scala/com/drzinks/scala/SparkFileProcessorApp.scala
+++ src/main/scala/com/drzinks/scala/SparkFileProcessorApp.scala
@@ -1,6 +1,6 @@
 package com.drzinks.scala
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 import java.io.File
 
@@ -12,10 +12,20 @@ class SparkFileProcessorApp {
       .appName("spark-file-processor")
       .getOrCreate();
 
-    val df = sparkSession.read.csv(file.getPath)
-    df.show(10,100,false)
-
+    val df = sparkSession
+      .read
+      .option("header","true")
+      .csv(file.getPath)
+    
+      filterTransformations(df)
+//    df.show(10,100,false)
 //    printFileContent(file)
+  }
+
+  private def filterTransformations(df: DataFrame): Unit = {
+    df.filter(row => row.getString(7).equalsIgnoreCase("textiles")).show(10,100,false) //Row => boolean
+    df.filter(df("Country") === "Poland").show(10,100,false) //condition: Column
+    df.filter("Founded == '1990'").show(false) //conditionExpr: String
   }
 
   private def printFileContent(file: File): Unit = {
